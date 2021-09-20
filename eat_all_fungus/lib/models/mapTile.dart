9187660552eb0c07ccc42e758843b8e9dbd7f950
Xanta55 <danielwaage@hotmail.de>
2021-09-20 13:09:13 +0200

    Added Tile Model and freezed

---
+++ eat_all_fungus/lib/models/mapTile.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'mapTile.freezed.dart';
+
+// *.g.dart is needed for fromJson/toJson
+part 'mapTile.g.dart';
+
+@freezed
+class MapTile with _$MapTile {
+  const factory MapTile({
+    required String description,
+    required List<String> inventory,
+    required bool isHidden,
+    required String world,
+    required int xCoord,
+    required int yCoord,
+    // The id will be generated in a certain way (we create our own - WORLD;xCoord;yCoord)
+    String? id,
+  }) = _MapTile;
+
+  factory MapTile.fromJson(Map<String, dynamic> json) =>
+      _$MapTileFromJson(json);
+
+  factory MapTile.fromDocument(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data()!;
+    return MapTile.fromJson(data).copyWith(id: doc.id);
+  }
+
+  Map<String, dynamic> toDocument() => toJson();
+}
