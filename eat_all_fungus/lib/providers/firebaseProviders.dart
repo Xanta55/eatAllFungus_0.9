5b56b27058fbb0a16f91508a56ae323541fa1c37
Xanta55 <danielwaage@hotmail.de>
2021-10-27 14:05:42 +0200

    Functions Init

--- eat_all_fungus/lib/providers/firebaseProviders.dart
+++ eat_all_fungus/lib/providers/firebaseProviders.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 final firebaseAuthProvider =
     Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
@@ -11,3 +12,6 @@ final databaseProvider =
 
 final fireStoreProvider =
     Provider<FirebaseStorage?>((ref) => FirebaseStorage.instance);
+
+final firebaseFunctionProvider =
+    Provider<FirebaseFunctions?>((ref) => FirebaseFunctions.instance);
