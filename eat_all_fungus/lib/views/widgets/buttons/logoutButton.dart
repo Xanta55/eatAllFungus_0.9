0a577fe6f6899cefcfe917abe71e4ed053bf048c
Xanta55 <danielwaage@hotmail.de>
2021-10-27 14:53:43 +0200

    QOLs

--- eat_all_fungus/lib/views/widgets/buttons/logoutButton.dart
+++ eat_all_fungus/lib/views/widgets/buttons/logoutButton.dart
@@ -1,5 +1,4 @@
 import 'package:eat_all_fungus/controllers/authController.dart';
-import 'package:eat_all_fungus/services/authRepository.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
